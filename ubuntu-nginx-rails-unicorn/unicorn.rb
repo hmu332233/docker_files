0e66ca4aeb37eb2ee6067df1cd2c9708a7ea0e3e
minung.han <hmu332233@gmail.com>
2018-04-29 12:08:57 +0900

    [Modify] unicorn default file

--- ubuntu-nginx-rails-unicorn/unicorn.rb
+++ ubuntu-nginx-rails-unicorn/unicorn.rb
@@ -7,6 +7,27 @@ pid "#{app_dir}/tmp/unicorn.pid"
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
-worker_processes 1
 listen "/tmp/unicorn.sock", :backlog => 64
-timeout 30
+
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
